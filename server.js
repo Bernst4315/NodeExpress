e225f5762f58e72e79f0a493202d84b932266081
Bernst4315 <bernst4315@gmail.com>
2025-01-24 21:33:14 -0700

    added routes

--- server.js
+++ server.js
@@ -6,8 +6,20 @@ const server = http.createServer((req, res) => {
     console.log("request made");
     res.setHeader("Content-Type", "text/html");
     
+    let path = "./views/";
+    switch(req.url){
+        case "/":
+            path += "index.html";
+            break;
+        case "/about":
+           path += "about.html";
+           break;
+        default:
+            path += "404.html";
+            break;
+    }
     //send an HTML file
-    fs.readFile("./views/index.html", (err, data) => {
+    fs.readFile(path, (err, data) => {
         if(err){
             console.log(err)
             res.end();
