c8b829eb6fe04b7f8b8e4fb9e03b1eff1e6672be
Bernst4315 <bernst4315@gmail.com>
2025-01-25 09:23:34 -0700

    added ejs, and webpages in ejs

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const app = express(); 
+
+//registar view engine
+app.set("view engine", "ejs");
+
 const port = 3000; 
 
 //listen for request
@@ -8,23 +12,35 @@ app.listen(port);
 app.get("/", (req,res) => {
 
     //res.send("<p>Home Page</p>")
-    res.sendFile("./views/index.html", {root: __dirname});
+    //res.sendFile("./views/index.html", {root: __dirname});
+    res.render("index");
 })
 
 app.get("/about", (req,res) => {
 
-    res.sendFile("./views/about.html", {root: __dirname});
+    //res.sendFile("./views/about.html", {root: __dirname});
    // res.send("<p>About Page</p>")
+   res.render("about");
 })
 
 //redirect
 
-app.get("/about-us", (req,res) => {
-    res.redirect("/about");
+// app.get("/about-us", (req,res) => {
+//     res.redirect("/about");
+// })
+
+//creat blogs page
+
+app.get("/blogs/create", (req,res) => {
+
+    //res.sendFile("./views/about.html", {root: __dirname});
+   // res.send("<p>About Page</p>")
+   res.render("create");
 })
 
 //404 page
 
 app.use((req,res) => {
-    res.status(404).sendFile("./views/404.html", {root: __dirname})
+    //res.status(404).sendFile("./views/404.html", {root: __dirname})
+    res.status(404).render("404");
 })
