808ead42c7e2a39b976ff06526d709f69c1e402b
Bernst4315 <bernst4315@gmail.com>
2025-01-24 21:23:25 -0700

    added html file

--- server.js
+++ server.js
@@ -1,8 +1,23 @@
 const http = require("http");
+const fs = require("fs"); //gets file syst
 const port = 3000; 
 
 const server = http.createServer((req, res) => {
     console.log("request made");
+    res.setHeader("Content-Type", "text/html");
+    
+    //send an HTML file
+    fs.readFile("./views/index.html", (err, data) => {
+        if(err){
+            console.log(err)
+            res.end();
+        }else {
+            //res.write(data)
+            res.end(data);//can add data var here if sending one thing
+        }
+    })// how to read a file
+
+
 }); //storing in var is optional
 
 //localhost is default. no need to add
