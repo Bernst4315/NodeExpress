32de3584baf44978bce9294e11e1e4a4ccafe4d5
Bernst4315 <bernst4315@gmail.com>
2025-01-25 19:05:01 -0700

    able to delet posts,  fixed: data no longer returns undefine

--- app.js
+++ app.js
@@ -150,7 +150,17 @@ app.get("/blogs/:id", (req,res) => {
     });
 });
 
+app.delete("/blogs/:id", (req, res) => {
+    const id = req.params.id;
 
+    Blog.findByIdAndDelete(id)
+    .then((result) => {
+      res.json({ redirect: '/blogs', });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+})
 
 
 //404 page
